aa5821dd3eef7aba443fd9ca939c0bf7c851481e
sakshi <sakshipandya162@gmail.com>
2024-06-13 14:53:33 +0530

    first commit

---
+++ ClassBasics/Program.cs
@@ -0,0 +1,93 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassBasics
+{
+    internal class Program
+    {
+        static void Main1()
+        {
+            //System.Console.WriteLine("Hello World");
+            //System.Console.ReadLine();
+            System.Console.WriteLine("H W");
+            System.Console.ReadLine();
+        }
+        static void Main2()
+        {
+            Console.WriteLine("H W");
+            Console.ReadLine();
+        }
+        static void Main3()
+        {
+            Class1 o; //o is a reference of type Class1
+            o = new Class1();  //new Class1() is an object of the class Class1 
+             // o refers to (points to) an object of type Class1
+            o.Display();
+            Console.ReadLine();
+        }
+        static void Main()
+        {
+            Class1 o= new Class1();  
+            o.Display();
+            o.Display("yash");
+            SqlConnection cn = new SqlConnection();
+
+
+            int ans = 0;
+            ans = o.Add();
+            Console.WriteLine(ans);
+            ans = o.Add(10);
+            Console.WriteLine(ans);
+            ans = o.Add(10, 20);
+            Console.WriteLine(ans);
+            ans = o.Add(10, 20, 30);
+            Console.WriteLine(ans);
+        }
+    }
+    public class Class1
+    {
+        public void Display()
+        {
+            Console.WriteLine("Display");
+        }
+        //function overloading
+        public void Display(string s)
+        {
+            Console.WriteLine("Display" + s);
+        }
+        //public int Add(int a, int b)
+        //{
+        //    return a + b;
+        //}
+        //public int Add(int a, int b, int c)
+        //{
+        //    return a + b + c;
+        //}
+
+        //default value for a parameter
+        public int Add(int a=0, int b=0, int c=0)
+        {
+            return a + b + c;
+        }
+    }
+    
+
+    
+}
+namespace MyNamespace
+{
+    public class C1 {
+    
+    }
+    public class C2 { }
+    namespace n2
+    {
+        public class C3 { } 
+    }
+
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+	public class Program
+	{
+		static void Main1(string[] args)
+		{
+			
+			StaticExample.pQ = 10456;
+			StaticExample obj = new StaticExample();
+			obj.i = 1;
+			obj.display();
+			//Calling  static method and variable by 
+
+
+
+		}
+
+	
+	}
+	//static member are accessed as classname.membername
+	public class StaticExample
+	{
+		public int i;
+		public static int si; //static variable - single copy for class
+
+		public void display()
+		{
+			Console.WriteLine(i);
+			Console.WriteLine(si);
+		}
+		public static void sDisplay()//static method - can call method directly without creating an object of class
+		{
+			//Console.WriteLine(i); 
+			//i can vary for different obj
+			//and we can not create object for static method so we can not say which object i refer too
+			Console.WriteLine(si);//this will work as si have single copy throughout
+			Console.ReadLine();
+		}
+
+		private static int pq;
+
+		public static int pQ
+		{
+			set {
+				if (pq > 9)
+					pq = value;
+				else
+				{
+					Console.WriteLine("Sorry we dont accept less of 9 value");
+				}
+			}
+			get {
+				return pq;
+			}
+		}
+		static StaticExample() // it is implicitly called by class so it does not have any other access specifier than ptivate
+		{
+			pQ = 90;
+			si = 1000;
+			Console.WriteLine("Static constructor is called first alwas");
+		}
+		public StaticExample()
+		{
+			Console.WriteLine("Not static constructor");
+		}
+	}
+
+
+	public  static class class1
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter a value ");
+			
+			
+			il = Convert.ToInt32(Console.ReadLine());
+			class1.display1();
+			Console.ReadLine();
+		}
+		static int il;
+
+		static void display1()
+		{
+			il = 9;
+		
+			if (il > 9)
+			{
+			
+				Console.WriteLine("No I dont accept this value i am sorry");
+			}
+			if(il < 10 && il != 9)
+			{
+				Console.WriteLine("Hatt ja yaha sw");
+			}
+			if(il == 9)
+			{
+				Console.WriteLine("Thank you i love you");
+			}
+		}
+
+	}
+}
